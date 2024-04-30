8e7acfe18b22ac1593ce8bf77b91bbddac38b515
ADIGA612 <h97abaza4pro@gmail.com>
2024-04-30 12:28:28 +0300

    feat: implement solver in controllers/sudoku-solver.js and unit tests in tests/1_unit-tests.js

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,143 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (puzzleString.length !== 81)
+      return "Expected puzzle to be 81 characters long";
+    if (puzzleString.match(/[^0-9.]/)) return "Invalid characters in puzzle";
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    let rows = puzzleString.match(/[1-9.]{9}/g);
+    let rowString = rows[row.toLowerCase().charCodeAt(0) - 97];
+    let ret = true;
+    if (rowString.match(value)) ret = false;
+    /* console.log(
+      "Row Placement:\t\tCOL:",
+      column,
+      "ROW:",
+      row,
+      " VALUE:",
+      value,
+      " Row STR:\t",
+      rowString,
+      "\tValid:",
+      ret
+    ); */
+    return ret;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    let columnString = puzzleString.replace(
+      /(.)(.)(.)(.)(.)(.)(.)(.)(.)/g,
+      "$" + column
+    );
+    let ret = true;
+    if (columnString.match(value)) ret = false;
+    /* console.log(
+      "Column Placement:\tCOL:",
+      column,
+      "ROW:",
+      row,
+      " VALUE:",
+      value,
+      " Column STR:\t",
+      columnString,
+      "\tValid:",
+      ret
+    ); */
+    return ret;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    row = row.toLowerCase().charCodeAt(0) - 97;
+    let cstart = Math.floor((column - 1) / 3) * 3;
+    let cend = cstart + 3;
+    let rstart = Math.floor(row / 3) * 3;
+    let rend = rstart + 3;
+    let region = puzzleString
+      .match(/[1-9.]{9}/g)
+      .slice(rstart, rend)
+      .map((row) => row.substring(cstart, cend))
+      .join("");
+    let ret = true;
+    if (region.match(value)) ret = false;
+   /*  console.log(
+      "Region Placement:\tCOL:",
+      column,
+      "ROW:",
+      row,
+      " VALUE:",
+      value,
+      " Region STR:\t",
+      region,
+      "\tValid:",
+      ret,
+      " Positions(3x3):\tCOL:",
+      cstart,
+      "-",
+      cend,
+      " | ROW:",
+      rstart,
+      "-",
+      rend
+    ); */
+    return ret;
   }
-
   solve(puzzleString) {
-    
+    try {
+      if (this.validate(puzzleString) === true) {
+        let tmpPuzzle = puzzleString;
+        const replaceAt = (str, ind, ch) =>
+          str.substring(0, ind) + ch + str.substring(++ind);
+        const findEmptyCell = () => {
+          let i = tmpPuzzle.indexOf(".");
+          // console.log("index: ", i);
+          if (i == -1) return false;
+          return {
+            row: String.fromCharCode(65 + Math.floor(i / 9)),
+            column: (i % 9) + 1,
+            index: i,
+          };
+        };
+        const isValid = (puzzle, row, column, value) => {
+          return (
+            this.checkColPlacement(puzzle, row, column, value) &&
+            this.checkRowPlacement(puzzle, row, column, value) &&
+            this.checkRegionPlacement(puzzle, row, column, value)
+          );
+        };
+        function recursive_solve(puzzleString) {
+          tmpPuzzle = puzzleString;
+          // console.log("TMP Puzzle:\t", tmpPuzzle);
+
+          if (!findEmptyCell()) {
+            return true;
+          }
+
+          const { row, column, index } = findEmptyCell();
+          for (let i = 1; i <= 9; i++) {
+            if (isValid(puzzleString, row, column, i)) {
+              tmpPuzzle = replaceAt(tmpPuzzle, index, i);
+              if (recursive_solve(tmpPuzzle)) {
+                return true;
+              }
+              tmpPuzzle = replaceAt(tmpPuzzle, index, ".");
+            }
+          }
+          return false;
+        }
+        if (recursive_solve(tmpPuzzle)) {
+          return tmpPuzzle;
+        } else {
+          return "Puzzle cannot be solved";
+        }
+      } else {
+        return this.validate(puzzleString);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,21 +2,101 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
 suite('Unit Tests', () => {
+    let puzzleString =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+    let invalidPuzzleString =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.311";
+    let incompletePuzzleString = puzzleString.substring(0, 80)
+    let solution =
+        "135762984946381257728459613694517832812936745357824196473298561581673429269145378";
+    let row = "A"
+    let column = "2"
+    let value = 3
 
-// Logic handles a valid puzzle string of 81 characters
-// Logic handles a puzzle string with invalid characters (not 1-9 or .)
-// Logic handles a puzzle string that is not 81 characters in length
-// Logic handles a valid row placement
-// Logic handles an invalid row placement
-// Logic handles a valid column placement
-// Logic handles an invalid column placement
-// Logic handles a valid region (3x3 grid) placement
-// Logic handles an invalid region (3x3 grid) placement
-// Valid puzzle strings pass the solver
-// Invalid puzzle strings fail the solver
-// Solver returns the expected solution for an incomplete puzzle
+    suite("Test Solver.validate(puzzleString)", () => {
+        // Logic handles a valid puzzle string of 81 characters
+        suite("Logic handles a valid puzzle string of 81 characters", () => {
+            assert.equal(solver.validate(puzzleString), true);
+        })
+
+        // Logic handles a puzzle string with invalid characters (not 1-9 or .)
+        suite("Logic handles a puzzle string with invalid characters (not 1-9 or .)", () => {
+            assert.equal(solver.validate(puzzleString.replace(".", "a")), "Invalid characters in puzzle");
+        })
+
+        // Logic handles a puzzle string that is not 81 characters in length
+        suite("Logic handles a puzzle string that is not 81 characters in length", () => {
+            assert.equal(solver.validate(puzzleString.substring(0, 80)), "Expected puzzle to be 81 characters long");
+        })
+
+    })
+
+    suite("Test Solver.checkRowPlacement()", () => {
+
+        // Logic handles a valid row placement
+        suite("Logic handles a valid row placement", () => {
+            assert.deepEqual(solver.checkRowPlacement(puzzleString, row, column, value), true);
+        })
+
+        // Logic handles an invalid row placement
+        suite("Logic handles an invalid row placement", () => {
+            assert.equal(solver.checkRowPlacement(puzzleString, "B", column, value), false);
+        })
+    })
+
+    suite("Test Solver.checkColPlacement()", () => {
+
+        // Logic handles a valid column placement
+        suite("Logic handles a valid column placement", () => {
+            assert.equal(solver.checkColPlacement(puzzleString, row, column, value), true);
+        })
+
+        // Logic handles an invalid column placement
+        suite("Logic handles an invalid column placement", () => {
+            assert.equal(solver.checkColPlacement(puzzleString, row, 1, value), false);
+        })
+    })
+
+    suite("Test Solver.checkRegionPlacement()", () => {
+
+        // Logic handles a valid region (3x3 grid) placement
+        suite("Logic handles a valid region (3x3 grid) placement", () => {
+            assert.equal(solver.checkRegionPlacement(puzzleString, row, column, value), true);
+        })
+
+        // Logic handles an invalid region (3x3 grid) placement
+        suite("Logic handles an invalid region (3x3 grid) placement", () => {
+            assert.equal(solver.checkRegionPlacement(puzzleString, row, column, 6), false);
+        })
+    })
+
+    suite("Test Solver.solve()", () => {
+
+        // Valid puzzle strings pass the solver
+        suite("Valid puzzle strings pass the solver", () => {
+            assert.equal(solver.solve(puzzleString), solution, "Valid puzzle strings should pass the solver");
+        })
+
+        // Invalid puzzle strings fail the solver
+        suite("Invalid puzzle strings fail the solver", () => {
+            assert.equal(
+                solver.solve(invalidPuzzleString),
+                "Puzzle cannot be solved",
+                "Invalid puzzle strings should fail the solver"
+            );
+        })
+
+        // Solver returns the expected solution for an incomplete puzzle
+        suite("Solver returns the expected solution for an incomplete puzzle", () => {
+            assert.equal(
+                solver.solve(incompletePuzzleString),
+                "Expected puzzle to be 81 characters long",
+                "Solver should returns the expected solution for an incomplete puzzle"
+            );
+        })
+    })
 
 });
